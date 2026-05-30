503bcd5fa1104df04e06b66177b6d8b8cb147cfa
User <user@example.com>
2026-05-30 12:14:09 +0400

    Configure Supabase project

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,4 +1,4 @@
 window.VLEARNS_SUPABASE_CONFIG = {
-  url: "",
-  anonKey: ""
+  url: "https://wzmurlwjfeppasjrqjol.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6bXVybHdqZmVwcGFzanJxam9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxMjQyNjAsImV4cCI6MjA5NTcwMDI2MH0.DRExmA0tGa3OExnmKdcfhOWdiphO7-6A3IWpPnFKdlI"
 };
